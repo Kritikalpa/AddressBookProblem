4935d34e2d451aead3f866b102a78756be1be477
Kriti Kalpa <krtklpsh@gmail.com>
2020-10-01 21:34:47 +0530

    [Kriti] Add. Create contact

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AddressBookSystem.Services;
+using System;
+using System.Text.RegularExpressions;
 
 namespace AddressBookSystem
 {
@@ -6,7 +8,45 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Problem");
+            Console.WriteLine("Welcome to Address Book Program");
+
+            Regex reEmail = new Regex(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z");
+            Regex rePhone = new Regex(@"^[0-9]{10}$");
+
+            Console.WriteLine("Create a contact");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Enter the first name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("\nEnter the last name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("\nEnter the address:");
+            string adddress = Console.ReadLine();
+            Console.WriteLine("\nEnter the city:");
+            string city = Console.ReadLine();
+            Console.WriteLine("\nEnter the state:");
+            string state = Console.ReadLine();
+            Console.WriteLine("\nEnter the zip:");
+            long zip = long.Parse(Console.ReadLine());
+            Console.WriteLine("\nEnter the email:");
+            string email = Console.ReadLine();
+            while (!reEmail.IsMatch(email))
+            {
+                Console.WriteLine("\nENTER A VALID EMAIL");
+                email = Console.ReadLine();
+            }
+            Console.WriteLine("\nEnter the phone number:");
+            long phoneNumber = long.Parse(Console.ReadLine());
+            while (!rePhone.IsMatch(phoneNumber.ToString()))
+            {
+                Console.WriteLine("\nENTER A VALID PHONE NUMBER");
+                phoneNumber = long.Parse(Console.ReadLine());
+            }
+
+            ContactPerson addressBook = new ContactPerson(firstName, lastName, adddress, city, state, email, zip, phoneNumber);
+
+            Console.WriteLine("\nADDRESS BOOK\n------------------");
+
+            addressBook.toString();
         }
     }
 }
---
+++ AddressBookSystem/Services/ContactPerson.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem.Services
+{
+    class ContactPerson
+    {
+        public string firstName, lastName, address, city, state, email;
+        public long zip, phoneNumber;
+
+        public ContactPerson(string firstName,string lastName, string address, string city, string state, string email, long zip, long phoneNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.email = email;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public void toString()
+        {
+            
+            Console.WriteLine(this.firstName + " " + this.lastName + ", " + this.address + ", " + this.city + ", " + this.state + ", " + this.zip + ", " + this.phoneNumber + ", " + this.email);
+        }
+    }
+}
