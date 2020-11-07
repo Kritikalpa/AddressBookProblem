d0e93fbe5a867f8c2c7a03d5c32246b1ccb0f2e8
Kriti Kalpa <krtklpsh@gmail.com>
2020-11-07 07:16:34 +0530

    [Kriti] Add. retrieve data based on city/state from DB

--- AddressBookSystem/retrieveContactSP.sql
+++ AddressBookSystem/retrieveContactSP.sql
@@ -5,4 +5,7 @@ IF @choice = 1
 BEGIN
 	SELECT id, first_name, last_name, address, city, state,email, zip, phone_number FROM addressBook
 END
-
+ELSE IF @choice = 2
+BEGIN
+	SELECT id, first_name, last_name, address, city, state,email, zip, phone_number FROM addressBook WHERE city ='LA' OR state = 'California';
+END
